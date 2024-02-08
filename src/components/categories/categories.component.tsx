8b9eeaf2726ae4c5ab67c30f222f1dcaa9be0092
Matheus Farias <matheusfariasdasneves@gmail.com>
2024-02-08 18:57:45 -0300

    feat: add Category Type

---
+++ src/components/categories/categories.component.tsx
@@ -0,0 +1,11 @@
+import './categories.styles.css'
+
+const Categories = () => {
+  return (
+    <div className='categories-container'>
+      <div className='categories-content'></div>
+    </div>
+  )
+}
+
+export default Categories
